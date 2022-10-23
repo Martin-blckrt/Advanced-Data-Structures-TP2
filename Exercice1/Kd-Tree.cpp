55db93bca56bb69240bcee00b93e20a1b7c3694f
Ergone <alexis.aragnouet@gmail.com>
2022-10-23 16:27:46 -0400

    ajout et recherche

--- Exercice1/Kd-Tree.cpp
+++ Exercice1/Kd-Tree.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 class Point {
 public:
+    string nom;
 	int x;
     int y;
 
-	Point(int pointX,int pointY) : x(pointX), y(pointY) {}
+	Point(string nomP,int pointX,int pointY) : nom(nomP), x(pointX), y(pointY) {}
 
 };
 
@@ -35,6 +36,7 @@ class KdTree { // comparaison commence en X puis Y,X,Y ect...
     inline void supprimerNoeud(KdNode* noeud);
     inline KdNode* chercherNoeud(Point* point);
     inline void plusProcheVoisin();
+    inline void afficherArbre();
 
     private:
         
@@ -65,9 +67,85 @@ void KdTree::ajouterNoeud(KdNode* node){
 }
 
 void KdTree::_ajouterNoeud(KdNode* nodeCompa,KdNode* nodeAjout){
-    
+    if(nodeCompa == nullptr){ // cas racine
+    racine = nodeAjout;
+    racine->hauteur = 0;
+    }
+    if(nodeCompa->hauteur % 2 == 0){ // comparaison sur X
+        if(nodeAjout->p->x < nodeCompa->p->x){ // à gauche
+            if(nodeCompa->gauche == nullptr){ // aucun élément à gauche, ajout du nouvel élément
+                nodeCompa->gauche = nodeAjout;
+                nodeAjout->hauteur = nodeCompa->hauteur+1;
+            } else { // un élément déjà existant à gauche, nouvelle comparaison
+                _ajouterNoeud(nodeCompa->gauche,nodeAjout);
+            }
+        } else { // à droite
+            if(nodeCompa->droit == nullptr){ // aucun élément à droite, ajout du nouvel élément
+                nodeCompa->droit = nodeAjout;
+                nodeAjout->hauteur = nodeCompa->hauteur+1;
+            } else { // un élément déjà existant à droite, nouvelle comparaison
+                _ajouterNoeud(nodeCompa->droit,nodeAjout);
+            }
+        }
+    } else { // comparaison sur Y
+        if(nodeAjout->p->y < nodeCompa->p->y){ // à gauche
+            if(nodeCompa->gauche == nullptr){ // aucun élément à gauche, ajout du nouvel élément
+                nodeCompa->gauche = nodeAjout;
+                nodeAjout->hauteur = nodeCompa->hauteur+1;
+            } else { // un élément déjà existant à gauche, nouvelle comparaison
+                _ajouterNoeud(nodeCompa->gauche,nodeAjout);
+            }
+        } else { // à droite
+            if(nodeCompa->droit == nullptr){ // aucun élément à droite, ajout du nouvel élément
+                nodeCompa->droit = nodeAjout;
+                nodeAjout->hauteur = nodeCompa->hauteur+1;
+            } else { // un élément déjà existant à droite, nouvelle comparaison
+                _ajouterNoeud(nodeCompa->droit,nodeAjout);
+            }
+        }
+    }
+
 }
 
 void KdTree::supprimerNoeud(KdNode* node){
     
+}
+
+KdNode* KdTree::chercherNoeud(Point* point){
+    KdNode* node = racine;
+    while(true){
+        if(node->p->x == point->x && node->p->y == point->y){ // si point trouvé
+                return node;
+            }
+        if(node->hauteur%2 == 0){ // test sur X
+            if(point->x < node->p->x){ // si plus petit sur X
+                if (node->gauche == nullptr){ // si pas d'élément à gauche
+                    return nullptr;
+                } else { // sinon on continue la recherche
+                    node = node->gauche;
+                }
+            } else { // si plus grand ou égal
+                if (node->droit == nullptr){ // si pas d'élément à droite
+                    return nullptr;
+                } else { // sinon on continue la recherche
+                    node = node->droit;
+                }
+            }
+        } else { // test sur Y
+            if(point->y < node->p->y){ // si plus petit sur Y
+                if (node->gauche == nullptr){ // pas d'élément à gauche
+                    return nullptr;
+                } else { // sinon on continue
+                    node = node->gauche;
+                }
+            } else { // si plus grand ou égal
+                if (node->droit == nullptr){ // si pas d'élément à droite
+                    return nullptr;
+                } else { // sinon on continue
+                    node = node->droit;
+                }
+            }
+        }
+    }
+    return nullptr;
 }
