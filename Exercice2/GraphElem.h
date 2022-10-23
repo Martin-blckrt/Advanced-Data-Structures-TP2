71c22c2846ccda56661e9298c06a397989937d2d
Thomas Sirvent <sirventhomas@gmail.com>
2022-10-23 15:24:32 -0400

    Basic graph elements

---
+++ Exercice2/GraphElem.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Edge 
+{
+public: 
+
+	Edge(int src, int dst): source(src), dest(dst){};
+
+	int getSource() { return source; };
+	int getDestination() { return dest; };
+
+private:
+	int source;
+	int dest;
+};
+
+class Node
+{
+public:
+
+	Node(int id, int dg, std::string lk):node_id(id), out_degree(dg), url(lk) {};
+
+	int getId() { return node_id; };
+	int getDegree() { return out_degree; };
+	std::string getUrl() { return url; };
+
+private:
+	int node_id;
+	int out_degree;
+
+	std::string url;
+
+};
+
