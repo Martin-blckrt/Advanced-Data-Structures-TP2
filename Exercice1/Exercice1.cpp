361b7345347a678800c64145418f76ca3dcc9c69
Ergone <alexis.aragnouet@gmail.com>
2022-11-22 23:45:58 -0500

    finition kdTree

--- Exercice1/Exercice1.cpp
+++ Exercice1/Exercice1.cpp
@@ -1,11 +1,63 @@
-// Exercice1.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <string>
+#include "Kd-Tree.cpp"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    KdTree* tree = new KdTree();
+    // Création des points 
+    Point* point1 = new Point("A",52,17);
+    Point* point2 = new Point("B",23,49);
+    Point* point3 = new Point("C",70,20);
+    Point* point4 = new Point("D",68,42);
+    Point* point5 = new Point("E",7,17);
+    Point* point6 = new Point("F",21,22);
+    Point* point7 = new Point("G",98,45);
+    Point* point8 = new Point("H",67,71);
+    Point* point9 = new Point("I",12,58);
+    Point* point10 = new Point("J",28,34);
+    Point* point12 = new Point("L",72,18);
+
+    // test ajout points
+    tree->ajouterNoeud(point1);
+    tree->ajouterNoeud(point2);
+
+    tree->afficherArbre();
+    printf("------------------------\n");
+
+    tree->ajouterNoeud(point3);
+    tree->ajouterNoeud(point4);
+    tree->ajouterNoeud(point5);
+    tree->ajouterNoeud(point6);
+    tree->ajouterNoeud(point7);
+    tree->ajouterNoeud(point8);
+    tree->ajouterNoeud(point9);
+    tree->ajouterNoeud(point10);
+    tree->ajouterNoeud(point12);
+
+    tree->afficherArbre();
+    printf("------------------------\n");
+    Point* point11 = new Point("K",12,58);
+    tree->ajouterNoeud(point11);
+    printf("------------------------\n");
+    tree->chercherNoeud(point1);
+    tree->chercherNoeud(point6);
+    tree->chercherNoeud(point10);
+
+    printf("------------------------\n");
+
+    tree->supprimerNoeud(tree->getNode(point2));
+    tree->afficherArbre();
+    printf("------------------------\n");
+    Point* point13 = new Point("M",22,70);
+    KdNode* node = tree->plusProcheVoisin(point13);
+    //printf("Point %d",node->p->x);
+    
+    //printf("Point %s aux coordonnees (%d,%d) \n",node->p->nom,node->p->x,node->p->y);
+
+    //cout << "Marche\n";
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
--- Exercice1/Kd-Tree.cpp
+++ Exercice1/Kd-Tree.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <cstring>
+#include <list>
+#include <math.h>
 
 using namespace std;
 
 class Point {
 public:
-    string nom;
+    char* nom;
 	int x;
     int y;
 
-	Point(string nomP,int pointX,int pointY) : nom(nomP), x(pointX), y(pointY) {}
+	Point(char* nomP,int pointX,int pointY) : nom(nomP), x(pointX), y(pointY) {}
 
 };
 
@@ -32,22 +33,32 @@ class KdTree { // comparaison commence en X puis Y,X,Y ect...
     inline explicit KdTree();
 	inline ~KdTree();
 
-    inline void ajouterNoeud(KdNode* noeud);
-    inline void supprimerNoeud(KdNode* noeud);
-    inline KdNode* chercherNoeud(Point* point);
-    inline void plusProcheVoisin();
+    inline void ajouterNoeud(Point* point);
+    inline KdNode* supprimerNoeud(KdNode*);
+    inline Point* chercherNoeud(Point* point);
+    inline KdNode* plusProcheVoisin(Point*);
     inline void afficherArbre();
+    inline KdNode* minNode(KdNode*,KdNode*,KdNode*,int);
+    inline KdNode* getNode(Point*);
 
     private:
         
     KdNode* racine;
+    int nbelem;
+    inline KdNode* _supprimerNoeud(KdNode* ,KdNode*);
     inline void _detruire(KdNode*);
-    inline void _ajouterNoeud(KdNode*,KdNode*);
+    inline void _ajouterNoeud(KdNode*,Point*);
+    inline KdNode* _bestNodeToReplace(KdNode*,int);
+    inline void _plusProcheVoisin(KdNode*,Point*,KdNode*,double*);
+    inline double _calculDistance(Point*,Point*);
+    inline void _afficherArbre(KdNode*);
+    inline KdNode* _getNode(KdNode*,Point*);
 
 };
 
 KdTree::KdTree(){
     racine = nullptr;
+    nbelem = 0;
 }
 
 KdTree::~KdTree(){
@@ -62,70 +73,185 @@ void KdTree::_detruire(KdNode* elem){
     }
 }
 
-void KdTree::ajouterNoeud(KdNode* node){
-    _ajouterNoeud(racine,node);
+void KdTree::ajouterNoeud(Point* PointS){
+    _ajouterNoeud(racine,PointS);
 }
 
-void KdTree::_ajouterNoeud(KdNode* nodeCompa,KdNode* nodeAjout){
+void KdTree::_ajouterNoeud(KdNode* nodeCompa,Point* PointS){
     if(nodeCompa == nullptr){ // cas racine
-    racine = nodeAjout;
+    KdNode* NouvNode = new KdNode(PointS);
+    racine = NouvNode;
     racine->hauteur = 0;
-    }
-    if(nodeCompa->hauteur % 2 == 0){ // comparaison sur X
-        if(nodeAjout->p->x < nodeCompa->p->x){ // à gauche
-            if(nodeCompa->gauche == nullptr){ // aucun élément à gauche, ajout du nouvel élément
-                nodeCompa->gauche = nodeAjout;
-                nodeAjout->hauteur = nodeCompa->hauteur+1;
-            } else { // un élément déjà existant à gauche, nouvelle comparaison
-                _ajouterNoeud(nodeCompa->gauche,nodeAjout);
+    nbelem++;
+    }else if(nodeCompa->p->x == PointS->x && nodeCompa->p->y == PointS->y){// identique
+        // fin
+        printf("point deja existant\n");
+    }else {
+        if(nodeCompa->hauteur % 2 == 0){ // comparaison sur X
+            if(PointS->x < nodeCompa->p->x){ // à gauche
+                if(nodeCompa->gauche == nullptr){ // aucun élément à gauche, ajout du nouvel élément
+                    KdNode* NouvNode = new KdNode(PointS);
+                    nodeCompa->gauche = NouvNode;
+                    NouvNode->hauteur = nodeCompa->hauteur+1;
+                    nbelem++;
+                } else { // un élément déjà existant à gauche, nouvelle comparaison
+                    _ajouterNoeud(nodeCompa->gauche,PointS);
+                }
+            } else { // à droite
+                if(nodeCompa->droit == nullptr){ // aucun élément à droite, ajout du nouvel élément
+                    KdNode* NouvNode = new KdNode(PointS);
+                    nodeCompa->droit = NouvNode;
+                    NouvNode->hauteur = nodeCompa->hauteur+1;
+                    nbelem++;
+                } else { // un élément déjà existant à droite, nouvelle comparaison
+                    _ajouterNoeud(nodeCompa->droit,PointS);
+                }
             }
-        } else { // à droite
-            if(nodeCompa->droit == nullptr){ // aucun élément à droite, ajout du nouvel élément
-                nodeCompa->droit = nodeAjout;
-                nodeAjout->hauteur = nodeCompa->hauteur+1;
-            } else { // un élément déjà existant à droite, nouvelle comparaison
-                _ajouterNoeud(nodeCompa->droit,nodeAjout);
+        } else { // comparaison sur Y
+            if(PointS->y < nodeCompa->p->y){ // à gauche
+                if(nodeCompa->gauche == nullptr){ // aucun élément à gauche, ajout du nouvel élément
+                    KdNode* NouvNode = new KdNode(PointS);
+                    nodeCompa->gauche = NouvNode;
+                    NouvNode->hauteur = nodeCompa->hauteur+1;
+                    nbelem++;
+                } else { // un élément déjà existant à gauche, nouvelle comparaison
+                    _ajouterNoeud(nodeCompa->gauche,PointS);
+                }
+            } else { // à droite
+                if(nodeCompa->droit == nullptr){ // aucun élément à droite, ajout du nouvel élément
+                    KdNode* NouvNode = new KdNode(PointS);
+                    nodeCompa->droit = NouvNode;
+                    NouvNode->hauteur = nodeCompa->hauteur+1;
+                    nbelem++;
+                } else { // un élément déjà existant à droite, nouvelle comparaison
+                    _ajouterNoeud(nodeCompa->droit,PointS);
+                }
             }
         }
-    } else { // comparaison sur Y
-        if(nodeAjout->p->y < nodeCompa->p->y){ // à gauche
-            if(nodeCompa->gauche == nullptr){ // aucun élément à gauche, ajout du nouvel élément
-                nodeCompa->gauche = nodeAjout;
-                nodeAjout->hauteur = nodeCompa->hauteur+1;
-            } else { // un élément déjà existant à gauche, nouvelle comparaison
-                _ajouterNoeud(nodeCompa->gauche,nodeAjout);
-            }
-        } else { // à droite
-            if(nodeCompa->droit == nullptr){ // aucun élément à droite, ajout du nouvel élément
-                nodeCompa->droit = nodeAjout;
-                nodeAjout->hauteur = nodeCompa->hauteur+1;
-            } else { // un élément déjà existant à droite, nouvelle comparaison
-                _ajouterNoeud(nodeCompa->droit,nodeAjout);
-            }
-        }
     }
-
 }
 
-void KdTree::supprimerNoeud(KdNode* node){
+KdNode* KdTree::supprimerNoeud(KdNode* nodeS){
+    return _supprimerNoeud(racine, nodeS);
+}
+
+
+KdNode* KdTree::_supprimerNoeud(KdNode* ActualNode,KdNode* nodeS){
+    // Given point is not present
+    if (ActualNode == nullptr){
+        return nullptr;
+    }    
+    // Find dimension of current node
+    int cd = ActualNode->hauteur % 2;
+    
+    // If the point to be deleted is present at root
+    if(nodeS == nullptr){
+        printf("test4\n");
+    }
+    if (ActualNode->p->x == nodeS->p->x && ActualNode->p->y == nodeS->p->y){
+        //printf("test4\n");
+        // 2.b) If right child is not NULL
+        if (ActualNode->droit != nullptr){
+            
+            // Find minimum of root's dimension in right subtree
+            KdNode *meilleureNode = _bestNodeToReplace(ActualNode->droit, cd);
+  
+            // Copy the minimum to root
+            ActualNode->p = meilleureNode->p;
+  
+            // Recursively delete the minimum
+            ActualNode->droit = _supprimerNoeud(ActualNode->droit, meilleureNode);
+        } else if (ActualNode->gauche != nullptr) { // same as above
+            KdNode *meilleureNode = _bestNodeToReplace(ActualNode->gauche, cd);
+            ActualNode->p = meilleureNode->p;
+            ActualNode->droit = _supprimerNoeud(ActualNode->gauche, meilleureNode);
+        } else {// If node to be deleted is leaf node
+            nbelem--;
+            delete ActualNode;
+            return nullptr;
+        }
+        return ActualNode;
+    } else {
+        
+    }
+    
+    // 2) If current node doesn't contain point, search downward
+    //printf("%d",cd);
+    if(cd==0){
+        
+        if (nodeS->p->x < ActualNode->p->x){
+            ActualNode->gauche = _supprimerNoeud(ActualNode->gauche, nodeS);
+        }else {
+            ActualNode->droit = _supprimerNoeud(ActualNode->droit, nodeS);
+        }
+    } else {
+        if (nodeS->p->y < ActualNode->p->y){
+            ActualNode->gauche = _supprimerNoeud(ActualNode->gauche, nodeS);
+        }else {
+            ActualNode->droit = _supprimerNoeud(ActualNode->droit, nodeS);
+        }
+    }
+    return ActualNode;
+}
+
+KdNode* KdTree::_bestNodeToReplace(KdNode* node,int dim){ // dim 0 ou 1
+    if(node == nullptr){
+        return nullptr;
+    } else {
+        if(node->hauteur%2 == dim){
+            if(node->gauche == nullptr){
+                return node;
+            } else {
+                return _bestNodeToReplace(node->gauche,dim);
+            }
+        } else {
+            return minNode(node,
+                    _bestNodeToReplace(node->gauche,dim),
+                    _bestNodeToReplace(node->droit,dim),
+                    dim);
+        }
+    }
     
 }
 
-KdNode* KdTree::chercherNoeud(Point* point){
+KdNode* KdTree::minNode(KdNode *main, KdNode *left, KdNode *right, int d){
+    KdNode *res = main;
+    if(d == 0){ // sur X
+        if (left != NULL && left->p->x < res->p->x){
+            res = left;
+        }
+        if (right != NULL && right->p->x < res->p->x){
+            res = right;
+        }
+    } else { // sur Y
+        if (left != NULL && left->p->y < res->p->y){
+            res = left;
+        }
+        if (right != NULL && right->p->y < res->p->y){
+            res = right;
+        }
+    }
+    return res;
+}
+
+Point* KdTree::chercherNoeud(Point* point){
     KdNode* node = racine;
     while(true){
         if(node->p->x == point->x && node->p->y == point->y){ // si point trouvé
-                return node;
+                printf("Point %s aux coordonnees (%d,%d) \n",node->p->nom,node->p->x,node->p->y);
+                return node->p;
             }
         if(node->hauteur%2 == 0){ // test sur X
             if(point->x < node->p->x){ // si plus petit sur X
                 if (node->gauche == nullptr){ // si pas d'élément à gauche
+                    printf("Point non trouve\n");
                     return nullptr;
                 } else { // sinon on continue la recherche
                     node = node->gauche;
                 }
             } else { // si plus grand ou égal
                 if (node->droit == nullptr){ // si pas d'élément à droite
+                    printf("Point non trouve\n");
                     return nullptr;
                 } else { // sinon on continue la recherche
                     node = node->droit;
@@ -134,12 +260,14 @@ KdNode* KdTree::chercherNoeud(Point* point){
         } else { // test sur Y
             if(point->y < node->p->y){ // si plus petit sur Y
                 if (node->gauche == nullptr){ // pas d'élément à gauche
+                    printf("Point non trouve\n");
                     return nullptr;
                 } else { // sinon on continue
                     node = node->gauche;
                 }
             } else { // si plus grand ou égal
                 if (node->droit == nullptr){ // si pas d'élément à droite
+                    printf("Point non trouve\n");
                     return nullptr;
                 } else { // sinon on continue
                     node = node->droit;
@@ -148,4 +276,112 @@ KdNode* KdTree::chercherNoeud(Point* point){
         }
     }
     return nullptr;
+}
+
+KdNode* KdTree::plusProcheVoisin(Point* point){
+
+    KdNode* meilleur = nullptr;   
+	double _minDist = std::numeric_limits<double>::max();
+
+    _plusProcheVoisin(racine,point,meilleur,&_minDist);
+    printf("Distance minimale : %2f \n",_minDist);
+    
+    return meilleur;
+}
+
+void KdTree::_plusProcheVoisin(KdNode* node,Point* point,KdNode* meilleur,double* mindist){
+    
+    if (node == nullptr){
+        return ;
+    }
+
+    const double dist = _calculDistance(point, node->p);
+    if (dist < *mindist){
+        *mindist = dist;
+        meilleur = node;
+        printf("Point %s aux coordonnees (%d,%d) \n",node->p->nom,node->p->x,node->p->y);
+    }
+    
+    if(node->hauteur%2 == 0){
+        if(point->x < node->p->x){
+            _plusProcheVoisin( node->gauche,point, meilleur, mindist);
+            const double diff = fabs(point->x - node->p->x);
+            if (diff < *mindist){
+                _plusProcheVoisin(node->droit, point, meilleur, mindist);
+            }
+        } else {
+            _plusProcheVoisin( node->droit,point, meilleur, mindist);
+            const double diff = fabs(point->x - node->p->x);
+            if (diff < *mindist){
+                _plusProcheVoisin(node->gauche, point, meilleur, mindist);
+            }
+        }
+        
+        
+    } else {
+        if(point->y < node->p->y){
+            _plusProcheVoisin( node->gauche,point, meilleur, mindist);
+            const double diff = fabs(point->y - node->p->y);
+            if (diff < *mindist){
+                _plusProcheVoisin(node->droit, point, meilleur, mindist);
+            }
+        } else {
+            _plusProcheVoisin( node->droit,point, meilleur, mindist);
+            const double diff = fabs(point->y - node->p->y);
+            if (diff < *mindist){
+                _plusProcheVoisin(node->gauche, point, meilleur, mindist);
+            }
+        }
+        
+    }
+        
+}
+
+double KdTree::_calculDistance(Point* p1,Point* p2){
+
+    return sqrt(pow(p2->x-p1->x,2)+pow(p2->y-p1->y,2));
+
+}
+
+void KdTree::afficherArbre(){
+    _afficherArbre(racine);
+}
+
+void KdTree::_afficherArbre(KdNode* node){
+    if (node != nullptr){
+        printf("Point %s aux coordonnees (%d,%d) \n",node->p->nom,node->p->x,node->p->y);
+        if(node->gauche != nullptr){
+            _afficherArbre(node->gauche);
+        }
+        if (node->droit != nullptr){
+            _afficherArbre(node->droit);
+        }
+    }  
+}
+
+KdNode* KdTree::getNode(Point* point){
+    return _getNode(racine,point);
+}
+
+KdNode* KdTree::_getNode(KdNode* node,Point* point){
+    if(node == nullptr){ // cas racine
+        return nullptr;
+    }else if(node->p->x == point->x && node->p->y == point->y){// identique
+        // fin
+        return node;
+    }else {
+        if(node->hauteur%2 == 0){
+            if(point->x < node->p->x){
+                return _getNode(node->gauche, point);
+            } else {
+                return _getNode(node->droit, point);
+            }
+        } else {
+            if(point->y < node->p->y){
+                return _getNode(node->gauche, point);
+            } else {
+                return _getNode(node->droit, point);
+            }
+        }
+    }
 }
